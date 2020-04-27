819fe7892f134b88b996e4456f94b511b37e8d71
Aleksei-Zaichenko <aleksei.zaichenko@cix.csi.cuny.edu>
2020-04-27 16:02:40 -0400

    Added user-model file and completed both post endpoints in authentication router

--- api/server.js
+++ api/server.js
@@ -3,6 +3,8 @@ const helmet = require('helmet');
 const cors = require('cors');
 require('dotenv').config()
 
+const authRouter = require('../auth/authentication-router.js');
+
 const server = express();
 
 server.use(helmet());
@@ -11,5 +13,7 @@ server.use(cors());
 
 server.use(helmet.hidePoweredBy({ setTo: process.env.HEADER }));
 helmet.frameguard({action: 'deny'});
+
+server.use('/api/auth', authRouter);
 
 module.exports = server;
--- auth/authentication-router.js
+++ auth/authentication-router.js
@@ -6,11 +6,41 @@ const Users = require('../users/users-model.js');
 
 // for endpoints beginning with /api/auth
 router.post('/register', (req, res) => {
-  
+
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 14); // 2 ^ n
+  user.password = hash;
+
+  Users.add(user)
+    .then(saved => {
+      res.status(201).json({message: 'a new user was added'});
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 });
 
 router.post('/login', (req, res) => {
-  
+  let { username, password } = req.body;
+
+  Users.findBy({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+
+        const token = generateToken(user);
+
+        res.status(200).json({
+          message: `Welcome ${user.username}!`,
+          token: token
+        });
+      } else {
+        res.status(401).json({ message: 'Invalid Credentials' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
 });
 
 function generateToken(user){
@@ -22,7 +52,7 @@ function generateToken(user){
   const secret = process.env.JWT_SECRET || 'keep it secret';
 
   const options ={
-    expiresIn: '2h'
+    expiresIn: '1h'
   }
 
   return jwt.sign(payload, secret, options);
