0d0e1b283b53bd3fe229a650f1e46fd0733bb380
Aleksei-Zaichenko <aleksei.zaichenko@cix.csi.cuny.edu>
2020-04-28 01:05:53 -0400

    changed production object in knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -21,15 +21,14 @@ module.exports = {
   },
 
   production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
     migrations: {
-      tableName: 'knex_migrations'
-    }
+      directory: "./data/migrations",
+    },
+    seeds: {
+      directory: "./data/seeds",
+    },
   },
 
   testing: {
