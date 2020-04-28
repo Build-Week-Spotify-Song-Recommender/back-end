09e93cf2f328a92b1bf76337460f2f608a5728c8
Aleksei-Zaichenko <aleksei.zaichenko@cix.csi.cuny.edu>
2020-04-28 18:29:56 -0400

    working on cors

--- api/server.js
+++ api/server.js
@@ -14,7 +14,7 @@ server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
-app.all('/', function(req, res, next) {
+app.use('/', function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     next()
