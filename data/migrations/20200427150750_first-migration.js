c776db0b67175884697f6087d0022736a63aa23b
Aleksei-Zaichenko <aleksei.zaichenko@cix.csi.cuny.edu>
2020-04-30 18:17:35 -0400

    working on bugs and typos

--- data/migrations/20200427150750_first-migration.js
+++ data/migrations/20200427150750_first-migration.js
@@ -1,65 +1,60 @@
+exports.up = function (knex) {
+  return (
+    knex.schema
 
-exports.up = function(knex) {
-  return knex.schema
-  
-    //database table for users
-    .createTable('users', tbl => {
+      //database table for users
+      .createTable("users", (tbl) => {
         tbl.increments();
 
-        tbl
-            .string('username', 128)
-            .notNullable()
-            .unique()
-            .index();
+        tbl.string("username", 128).notNullable().unique().index();
 
-        tbl
-        .string('emailAddress', 128)
-        .notNullable()
-        .unique();
-        
-        tbl.string('password', 128).notNullable();
-    })
+        tbl.string("emailAddress", 128).notNullable().unique();
 
-    //database table for songs
-    .createTable('songs', tbl => {
+        tbl.string("password", 128).notNullable();
+      })
+
+      //database table for songs
+      .createTable("songs", (tbl) => {
         tbl.increments();
 
-        tbl.string('title', 128).notNullable().index();
-        tbl.string('artist', 128).notNullable().index();
-        tbl.string('album', 128).notNullable();
-        tbl.string('album_cover_art').notNullable();
-    })
+        tbl.string("title", 128).notNullable().unique().index();
+        tbl.string("artist", 128).notNullable().index();
+        tbl.string("album", 128).notNullable();
+        tbl.string("album_cover_art").notNullable();
+      })
 
-    //database table for saved songs
-    .createTable('savedSongs', tbl => {
+      //database table for saved songs
+      .createTable("savedSongs", (tbl) => {
         tbl.increments();
 
         //foreign key
         tbl
-            .integer('user_id')
-            .unsigned()
-            .notNullable()
-            .references('id')
-            .inTable('users')
-            .onDelete('CASCADE')
-            .onUpdate('CASCADE');
+          .integer("user_id")
+          .unsigned()
+          .notNullable()
+          .references("id")
+          .inTable("users")
+          .onDelete("CASCADE")
+          .onUpdate("CASCADE");
 
         //foreign key
         tbl
-            .integer('song_id')
-            .unsigned()
-            .notNullable()
-            .references('id')
-            .inTable('songs')
-            .onDelete('CASCADE')
-            .onUpdate('CASCADE');
+          .integer("song_id")
+          .unsigned()
+          .notNullable()
+          .references("id")
+          .inTable("songs")
+          .onDelete("CASCADE")
+          .onUpdate("CASCADE");
 
-        tbl.unique(['user_id', 'song_id']);
-    });
+        tbl.unique(["user_id", "song_id"]);
+      })
+  );
 };
 
-exports.down = function(knex) {
-    return knex.schema.dropTableIfExists('savedSongs')
-                       .dropTableIfExists('songs')
-                       .dropTableIfExists('users');
+exports.down = function (knex) {
+  return knex.schema
+    .dropTableIfExists("savedSongs")
+    .dropTableIfExists("songs")
+    .dropTableIfExists("users");
 };
