7b6f8c987c17dd30fd718f25196deff10cb5e0a9
Aleksei-Zaichenko <aleksei.zaichenko@cix.csi.cuny.edu>
2020-04-30 23:00:53 -0400

    working on saving liked song to favorite songs

--- savedSongsForEachUser/savedSongs-model.js
+++ savedSongsForEachUser/savedSongs-model.js
@@ -1,39 +1,40 @@
-const db = require('../data/db-config.js');
+const db = require("../data/db-config.js");
 
 module.exports = {
   add,
   getAllSavedSongsForUser,
   findBy,
   findById,
-  removeSongFromSaved
+  removeSongFromSaved,
 };
 
 function getAllSavedSongsForUser(passedUserId) {
-    return db('savedSongs')
-        .where('user_id', passedUserId)
-        .join('users', 'savedSongs.user_id', 'users.id')
-        .join('songs', 'savedSongs.song_id', 'songs.id')
-        .select('songs.title','songs.artist', 'songs.album', 'songs.album_cover_art');
-  }
-
-function findBy(filter) {
-  return db('savedSongs').where(filter);
+  return db("savedSongs")
+    .where("user_id", passedUserId)
+    .join("users", "savedSongs.user_id", "users.id")
+    .join("songs", "savedSongs.song_id", "songs.id")
+    .select(
+      "songs.title",
+      "songs.artist",
+      "songs.album",
+      "songs.album_cover_art"
+    );
 }
 
-async function add(passedConnection) {
-  const [id] = await db('savedSongs').insert(passedConnection);
+function findBy(filter) {
+  return db("savedSongs").where(filter);
+}
 
-  return findById(id);
+function add(passedConnection) {
+  return db("savedSongs").insert(passedConnection);
 }
 
 function findById(id) {
-  return db('savedSongs')
-    .where({ id })
-    .first();
+  return db("savedSongs").where({ id }).first();
 }
 
 function removeSongFromSaved(passedUserId, passedSongId) {
-    return db('savedSongs')
-      .where({user_id: passedUserId, song_id:passedSongId })
-      .del();
+  return db("savedSongs")
+    .where({ user_id: passedUserId, song_id: passedSongId })
+    .del();
 }
