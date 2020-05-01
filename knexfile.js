a2db21edba02d2ebc92c01f3ebc365be586690e6
Aleksei-Zaichenko <aleksei.zaichenko@cix.csi.cuny.edu>
2020-04-30 20:12:41 -0400

    still working on registration issues

--- knexfile.js
+++ knexfile.js
@@ -1,9 +1,28 @@
-module.exports = {
+const { Client } = require("pg");
 
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true,
+});
+
+client.connect();
+
+client.query(
+  "SELECT table_schema,table_name FROM information_schema.tables;",
+  (err, res) => {
+    if (err) throw err;
+    for (let row of res.rows) {
+      console.log(JSON.stringify(row));
+    }
+    client.end();
+  }
+);
+
+module.exports = {
   development: {
-    client: 'sqlite3',
+    client: "sqlite3",
     connection: {
-      filename: './data/users.db3'
+      filename: "./data/users.db3",
     },
     useNullAsDefault: true,
     migrations: {
@@ -15,13 +34,13 @@ module.exports = {
     pool: {
       // runs after a connection is made to the sqlite engine
       afterCreate: (conn, done) => {
-        conn.run('PRAGMA foreign_keys = 0', done);
+        conn.run("PRAGMA foreign_keys = 0", done);
       },
     },
   },
 
   production: {
-    client: 'postgresql',
+    client: "pg",
     connection: process.env.DATABASE_URL,
     migrations: {
       directory: "./data/migrations",
